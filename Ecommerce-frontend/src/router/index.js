73d6284fd52ac3c24f84c0e236aec5be51e061ab
har-ga <harshgarg1399@gmail.com>
2023-08-24 14:01:50 +0530

    wishListRemoved

--- Ecommerce-frontend/src/router/index.js
+++ Ecommerce-frontend/src/router/index.js
@@ -9,7 +9,6 @@ import EditProduct from "../views/Product/EditProduct";
 import ShowDetails from "../views/Product/ShowDetails";
 import Signup from "../views/Signup";
 import Signin from "../views/Signin";
-// import WishList from "../views/Product/WishList";
 import Cart from "../views/cart/Cart";
 
 const routes = [
